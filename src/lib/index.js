2ddaeed200416428d8fc3719211f38f7592ed4fb
2586224896 <2586224896@qq.com>
2020-10-16 14:02:01 +0800

    替换组件库

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,30 +1,23 @@
 import btn from './button/button.vue'
 import grouplabel from './groupLabel/groupLabel.vue'
-import timeLabel from './timeLabel/timeLabel.vue'
-// import VEcharts from './VEcharts/VEcharts.vue'
+// import timeLabel from './timeLabel/timeLabel.vue'
 import YtoTable from './YtoTable/YtoTable.vue'
 import scroller from './scroller/scroller.vue'
-const components = {
-    btn,
-    grouplabel
-}
+import newDate from './newDate/newDate.vue'
+
 const jsc = {}
 // 这一步判断window.Vue是否存在，因为直接引用vue.min.js， 它会把Vue绑到Window上，我们直接引用打包好的js才能正常跑起来。
 if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.component('yyl-npm-practice', btn)
-    // window.Vue.component('yyl-npm-practice', groupLabel)
 }
 //这样就可以使用Vue.use进行全局安装了。
 jsc.install = function (Vue) {
-    // for (let item in components) {
-    //     Vue.component(item.name, item)
-    // }
     Vue.component(btn.name, btn)
     Vue.component(grouplabel.name, grouplabel)
-    Vue.component(timeLabel.name, timeLabel)
-    // Vue.component(VEcharts.name, VEcharts)
+    // Vue.component(timeLabel.name, timeLabel)
     Vue.component(YtoTable.name, YtoTable)
     Vue.component(scroller.name, scroller)
+    Vue.component(newDate.name,newDate)
 }
 
 export default jsc
---
+++ src/lib/newDate/newDate.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="time-label flx-ce-end"
+            :class="{'link-sty':isLink}"
+            @click="toggleTime">
+            <slot>test 2020</slot>
+        </div>
+        <yto-date-range ref="picker"
+                :dateType="dateType=='day'?'date':dateType"
+                :value="dateVal"
+                @confirm="confirmDate"></yto-date-range>
+    </div>
+</template>
+<script>
+export default {
+    name: 'jsc-time',
+    // isLink 是否是链接 链接标记文案为颜色
+    props: ['isLink', 'dateType', 'dateVal', 'endDate', 'stepDay'],
+    data () {
+        return {
+
+        };
+    },
+    mounted () {
+
+    },
+    methods: {
+        // 切换日期形式 年月 年
+        toggleTime () {
+            if (!this.isLink) return;
+            this.$emit('togTime');
+            this.$refs['picker'].open();
+        },
+        // 选择时间
+        confirmDate (val) {
+            this.$emit('confirmDate', val);
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.time-label{
+    width:100%;
+    padding: 20px 30px;
+    font-size:26px;
+    color:rgba(153,153,153,1);
+}
+.link-sty{
+    color:#2482FC
+}
+</style>
